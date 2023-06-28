6a33fe89057f53a49a3428825d4b5ea5b2982c04
JuanDBta <juandavid_diaz@hotmail.com>
2023-06-28 13:39:53 -0500

    Add delete button and submit button

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 const TodosLogic = () => {
-    const todos = [
+  const [todos, setTodos] = useState([
       {
         id: 1,
         title: 'Setup development environment',
@@ -18,13 +19,42 @@ const TodosLogic = () => {
         title: 'Deploy to live server',
         completed: false,
       },
-    ];
+    ]); const handleChange = (id) => {
+      setTodos((prevState) =>
+        prevState.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            };
+          }
+          return todo;
+        })
+      );
+    };
+    const delTodo = (id) => {
+      setTodos([
+        ...todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ]);
+    };
+    const addTodoItem = (title) => {
+      const newTodo = {
+        id: 4,
+        title: title,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+    };
+    
     return (
       <div>
-        <InputTodo />
-        <TodosList todosProps={todos} />
+        <InputTodo addTodoItem={addTodoItem} />
+        <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
       </div>
     );
+
   };
   export default TodosLogic;
   
